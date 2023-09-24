0f8a2bc783328f7180f98aab539fa4da6acb4b1f
Neovitalism <neovitalism@gmail.com>
2023-09-23 22:07:19 -0400

    Initial commit

---
+++ forge116/src/main/java/me/neovitalism/neomixins/NeoMixins.java
@@ -0,0 +1,8 @@
+package me.neovitalism.neomixins;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("neomixins")
+public class NeoMixins {
+
+}
---
+++ forge120/src/main/java/me/neovitalism/neomixins/NeoMixins.java
@@ -0,0 +1,6 @@
+package me.neovitalism.neomixins;
+
+
+public class NeoMixins {
+
+}
